0c5f8d1bdf2684ec5acaf2aa4d8961a4f0c7fc07
Gabriel Francisco <gf.bezerra10@gmail.com>
2026-08-18 15:19:15 -0300

    Refactor and complete exercises in C program

--- Primeira Lista de Exercicios.c
+++ Primeira Lista de Exercicios.c
@@ -94,6 +94,41 @@ int main(int argc, char *argv[]) {
 	quantidadedias = resto % 30; 
 		
 	printf("Voce nasceu faz %d anos, %d meses e %d dias", quantidadeanos, quantidademeses, quantidadedias);
+
+	
+//Exercício 7:
+
+	
+	double volume, raio;
+   	double pi = 3.14159;
+	
+	printf("Qual o valor do raio da esfera?: ");
+	scanf("%lf", &raio);
+	
+	pi = 3.14159;
+	
+	volume = (4.0 / 3.0)*pi*raio*raio*raio;
+	
+	printf("O volume da esfera e igual a: %lf \n ", volume);
+	
+	
+//Exercício 8: 
+
+	
+	double x1,y1,x2,y2, distancia;
+	
+	printf("Digite a coordenada x1: ");
+	scanf("%lf", &x1);
+	printf("Digite a coordenada y1: ");
+	scanf("%lf", &y1);
+	printf("Digite a coordenada x2: ");
+	scanf("%lf", &x2);
+	printf("Digite a coordenada y2: ");
+	scanf("%lf", &y2);
+	
+	distancia = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	
+	printf("A distancia entre os valores sao de: %f ", distancia);
 	
 	
 	
