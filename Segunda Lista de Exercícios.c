c6650ff6e1ce762b33e8f0a8eb70759e6edbece9
Gabriel Francisco <gf.bezerra10@gmail.com>
2026-08-18 16:03:05 -0300

    Fix formatting in Segunda Lista de Exercícios.c

---
+++ Segunda Lista de Exercícios.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+//Exercício 1
+
+	int anodenasc, idade, anoatual;
+	
+	printf("Quantos anos voce tem?: ");
+	scanf("%d", &idade);
+	printf("Em qual ano estamos?: ");
+	scanf("%d", &anoatual);
+	
+	anodenasc = anoatual - idade;
+	
+	printf("Voce nasceu no ano de: %d ", anodenasc);	
+	
+	
+//Exercício 2
+
+	
+	
+	
+	
+	
+}
