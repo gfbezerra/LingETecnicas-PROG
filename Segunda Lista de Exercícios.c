03947fe7493f13d63e1166732afcdd5f6b6ae5cd
Gabriel Francisco <gf.bezerra10@gmail.com>
2026-08-20 11:15:58 -0300

    Implement function to determine the largest of four numbers
    Added logic to find the largest of four integers.

--- Segunda Lista de Exercícios.c
+++ Segunda Lista de Exercícios.c
@@ -21,6 +21,27 @@ int main(int argc, char *argv[]) {
 	
 //Exercício 2
 
+
+
+
+
+//Exercício 10:
+
+	int a, b, c, d, maior, maior_temp1, maior_temp2;
+	printf("Informe os valores a serem comparados: ");
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	
+	//conta
+	maior_temp1 = (a+b+abs(a-b))/2;
+	maior_temp2 = (maior_temp1+c+abs(maior_temp1-c))/2;
+	maior = (maior_temp2 +d+abs(maior_temp2-d))/2;
+	
+
+
+	printf("O maior entre |%d|%d|%d|%d| = %d", a,b,c,d, maior);
+	
+	return 0;
+
 	
 	
 	
