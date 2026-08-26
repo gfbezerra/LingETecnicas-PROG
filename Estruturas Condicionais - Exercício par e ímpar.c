6b94b7cd4119a9e88433c2a5d34af0cdd7fdad03
Gabriel Francisco <gf.bezerra10@gmail.com>
2026-08-26 11:17:56 -0300

    Add files via upload

---
+++ Estruturas Condicionais - Exercício par e ímpar.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a, b, c, r;
+	
+	printf("Entre com os valores para A B C: ");
+	scanf("%d %d %d", &a, &b, &c);
+	
+	if (a>b){
+		r = a;
+	}else 
+		r = b;
+	if(c>r){
+		r = c;
+	}
+	
+	printf("%d eh o maior", r);
+	
+	
+//ÍMPAR OU PAR:	
+	
+	int a, par, impar;
+	
+	printf("Digite um valor: ");
+	scanf("%d", &a);
+	
+	if (a % 2 != 0){
+		a = impar;
+		printf("O número é IMPAR!");
+	}
+	else{
+		a = par;
+		printf("O número é PAR!");	
+	} 
+
+	
+	
+	return 0;
+}
